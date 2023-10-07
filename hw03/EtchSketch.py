c290f9db65944c358e14ca0ed404958432191880
Beagle User <debian@BeagleBone.localdomain>
2023-10-07 23:05:22 +0000

    In progress, TMP101 Complete, EtchSketch in progress 1/2

---
+++ hw03/EtchSketch.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#Read a TMP101 sensor
+#//////////////////////////////////////
+#Author : Jailen Hobbs
+#Program: Reads from address 0x48 and 0x49 of I2C bus 2
+#HArdware configuration to pins 19 and 20 with one TMP101 GND and one floating
+#
+
+import smbus
+import time
+
+#i2c bus and addresses
+bus = smbus.SMBus(2)
+address = 0x48
+address2 = 0x49
+
+while True:
+	#Get temp of both TMP101 sensors
+	temp = bus.read_byte_data(address, 0)
+	temp2 = bus.read_byte_data(address, 0)
+	print(str(temp)+"-Sensor1 "+ str(temp2) + "-Sensor 2", end = "\r")
+	time.sleep(0.1)
