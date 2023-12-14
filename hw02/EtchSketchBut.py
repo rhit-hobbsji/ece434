83bb32277e3cc6ee694c1718a5a755e1574d10d2
Hobbs <hobbsji@rose-hulman.edu>
2023-12-14 00:11:37 -0500

    Work pls

--- hw02/EtchSketchBut.py
+++ hw02/EtchSketchBut.py
@@ -14,8 +14,55 @@
 import gpiod
 import sys
 
+
+def create_board():
+    input1 = input("Enter Board size (NxN): ")
+    size = int(input1)
+    print("Board Size " + input1 + " x " + input1)
+
+    global max_index, row, col, min_index
+    row, col = 0, 0
+    min_index = 0
+    max_index = size - 1
+
+    array = [["-" for i in range(size)] for k in range(size)]
+    return array
+
+def move_left():
+    global row
+    row = row - 1
+    if (row < min_index):
+        row = max_index
+
+def move_right():
+    global row
+    row = row + 1
+    if (row > max_index):
+        row = min_index
+
+def move_up():
+    global col
+    col = col - 1
+    if (col < min_index):
+        col = max_index
+
+def move_down():
+    global col
+    col = col + 1
+    if (col > max_index):
+        col = min_index
+
+def clear():
+    global array
+    for i in range(len(array)):
+        for k in range(len(array)):
+            array[i][k] = "-"
+    
+
 CONSUMER='getset'
 CHIP='1'
+
+cleargetoffsets=[12] #P8_12
 leftgetoffsets=[10] # P8_14
 rightgetoffsets=[15] # P8_15
 upgetoffsets=[14] # P8_16
@@ -24,6 +71,8 @@ leftsetoffests=[18] # P9_14
 rightsetoffests=[19] # P9_16
 upsetoffests=[9] # P8_13
 downsetoffests=[8] # P8_19
+clearsetoffsets=[31] # P8_26
+
 
 
 
@@ -41,24 +90,107 @@ def print_event(event):
 
 chip = gpiod.Chip(CHIP)
 
-getlines = chip.get_lines(getoffsets)
-getlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+lgetlines = chip.get_lines(leftgetoffsets)
+lgetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+rgetlines = chip.get_lines(rightgetoffsets)
+rgetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+ugetlines = chip.get_lines(upgetoffsets)
+ugetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+dgetlines = chip.get_lines(downgetoffsets)
+dgetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+cgetlines = chip.get_lines(cleargetoffsets)
+cgetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
 
-setlines = chip.get_lines(setoffests)
-setlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
+lsetlines = chip.get_lines(leftsetoffests)
+lsetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
+rsetlines = chip.get_lines(rightsetoffests)
+rsetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
+usetlines = chip.get_lines(upsetoffests)
+usetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
+dsetlines = chip.get_lines(downsetoffests)
+dsetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
+csetlines = chip.get_lines(clearsetoffsets)
+csetlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
 
 print("Hit ^C to stop")
 
-while True:
-    ev_lines = getlines.event_wait(sec=1)
-    if ev_lines:
-        for line in ev_lines:
-            event = line.event_read()
-            print_event(event)
-    vals = getlines.get_values()
+global array
+array = create_board()
+x = 0
 
+for r in array:
+        for element in r:
+            print(element, end=" ")
+        print()
+
+vals = []
+
+while True:
+    x = 0
+    
+    lev_lines = lgetlines.event_wait(sec=1)
+    rev_lines = rgetlines.event_wait(sec=1)
+    uev_lines = ugetlines.event_wait(sec=1)
+    dev_lines = dgetlines.event_wait(sec=1)
+    cev_lines = usetlines.event_wait(sec=1)
+    
+    if lev_lines:
+        for line in lev_lines:
+            event = line.event_read()
+            #print_event(event)
+    vals[0] = lgetlines.get_values()
+    
+    if rev_lines:
+        for line in rev_lines:
+            event = line.event_read()
+            #print_event(event)
+    vals[1] = rgetlines.get_values()
+    if uev_lines:
+        for line in uev_lines:
+            event = line.event_read()
+            #print_event(event)
+    vals[2] = ugetlines.get_values()
+    
+    if dev_lines:
+        for line in dev_lines:
+            event = line.event_read()
+            #print_event(event)
+    vals[3] = dgetlines.get_values()
+    
+    if cev_lines:
+        for line in cev_lines:
+            event = line.event_read()
+            #print_event(event)
+    vals[4] = cgetlines.get_values()
+    
     # for val in vals:
     #     print(val, end=' ')
     # print('\r', end='')
 
-    setlines.set_values(vals)
+    lsetlines.set_values(vals[0])
+    rsetlines.set_values(vals[1])
+    usetlines.set_values(vals[2])
+    dsetlines.set_values(vals[3])
+    csetlines.set_values(vals[4])
+    
+    if vals[0]:
+        move_left()
+    elif vals[1]:
+        move_right()
+    elif vals[2]:
+        move_up()
+    elif vals[3]:
+        move_down()
+    elif vals[4]:
+        clear()
+        x = 1
+    
+    if(x != 1):
+        array[col][row] = "x" 
+    
+    for r in array:
+        for element in r:
+            print(element, end=" ")
+        print()
+    
+    
