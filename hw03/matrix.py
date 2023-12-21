a7968c3b4edd993ca7be27414f58f1e0bde5c863
Hobbs <hobbsji@rose-hulman.edu>
2023-12-21 11:39:19 -0500

    matrix

--- hw03/matrix.py
+++ hw03/matrix.py
@@ -5,6 +5,21 @@
 import smbus
 import time
 
+def move_left():
+    print("left")
+
+def move_right():
+    print("right")
+
+def move_up():
+    print("up")
+
+def move_down():
+    print("down")
+
+def clear():
+    print("clear")
+
 bus = smbus.SMBus(2)  # Use i2c bus 1
 matrix = 0x70         # Use address 0x70
 tmp1 = 0x48 #sensor 1
@@ -39,11 +54,11 @@ f.write(startCount)
 f.close()
 
 #enable Rotaryencoders
-f = open(pathe1+'enable', 'w')
+f = open(pathe1+'/enable', 'w')
 f.write('1')
 f.close()
 
-f = open(pathe2+'enable', 'w')
+f = open(pathe2+'/enable', 'w')
 f.write('1')
 f.close()
 
@@ -52,10 +67,10 @@ l = open(pathe1+'/count', 'r')
 r = open(pathe1+'/count', 'r')
 
 horzEncoderOldData = 5000
-horzEnoderCurData = 5000
+horzEncoderCurData = 5000
 
 vertEncoderOldData = 5000
-vertEnoderCurData = 5000
+vertEncoderCurData = 5000
 
 bus.write_byte_data(matrix, 0x21, 0)   # Start oscillator (p10)
 bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
@@ -66,11 +81,34 @@ array = [0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85,
     0x04, 0x85, 0x28, 0x89, 0x00, 0x42, 0x00, 0x3c
 ]
 
+curPos = [0x0,1]
+
 bus.write_i2c_block_data(matrix, 0, array)
 
 while True:
-    
     temp = bus.read_byte_data(tmp1, 0)
     temp2 = bus.read_byte_data(tmp2, 0)
+    r.seek(0)
+    l.seek(0)
+    
+    vertEnoderCurData = r.read()
+    horzEnoderCurData = l.read()
+    
+    horzData = (horzEnoderCurData - horzEncoderOldData)
+    vertData = (vertEnoderCurData - vertEncoderOldData)
+    if(horzData > 0):
+        move_right()
+    elif (horzData < 0):
+        move_left()
+    elif(vertData > 0):
+        move_up()
+    elif(vertData < 0):
+        move_down()   
+     
+    if(temp > 28) | (temp2 > 28):
+        print("TOO HOT GAME OVER INPUTS STUCK")
+        break
     bus.write_i2c_block_data(matrix, 0, array)
+    vertEncoderOldData = vertEncoderCurData
+    horzEncoderOldData = horzEncoderCurData
 
