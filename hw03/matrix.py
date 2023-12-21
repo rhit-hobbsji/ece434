7bf6c95310065f565ec8f8e2bd35405b80e9984a
Hobbs <hobbsji@rose-hulman.edu>
2023-12-21 14:22:41 -0500

    matrix

--- hw03/matrix.py
+++ hw03/matrix.py
@@ -7,6 +7,8 @@ import time
 
 def move_left():
     print("left")
+    
+    
 
 def move_right():
     print("right")
@@ -62,9 +64,9 @@ f = open(pathe2+'/enable', 'w')
 f.write('1')
 f.close()
 
+#initial rotary numbers
 horzEncoderOldData = 5000
 horzEncoderCurData = 5000
-
 vertEncoderOldData = 5000
 vertEncoderCurData = 5000
 
@@ -73,8 +75,8 @@ bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
 bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
 
 # The first byte is GREEN, the second is RED.
-array = [0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85,
-    0x04, 0x85, 0x28, 0x89, 0x00, 0x42, 0x00, 0x3c
+array = [0x01, 0x00, 0x02, 0x00, 0x04, 0x00, 0x08, 0x00,
+    0x10, 0x00, 0x20, 0x00, 0x40, 0x00, 0x80, 0x00
 ]
 
 curPos = [0x0,1]
