3e5f3bb773131db63b4db4db43a8a595573f5ba2
Hobbs <hobbsji@rose-hulman.edu>
2023-12-21 14:26:08 -0500

    matrix

--- hw03/matrix.py
+++ hw03/matrix.py
@@ -8,8 +8,6 @@ import time
 def move_left():
     print("left")
     
-    
-
 def move_right():
     print("right")
 
@@ -34,7 +32,7 @@ pathe1 = '/dev/bone/counter/' +eQEP1 + '/count0'
 eQEP2 = '2'
 pathe2 = '/dev/bone/counter/' +eQEP2 + '/count0'
 
-delay = 0.1; # Delay between images in s
+delay = 0.3; # Delay between images in s
 
 #Set Ceiling values
 startCount = '5000'
@@ -75,11 +73,11 @@ bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
 bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
 
 # The first byte is GREEN, the second is RED.
-array = [0x01, 0x00, 0x02, 0x00, 0x04, 0x00, 0x08, 0x00,
+array = [0x01, 0x03, 0x02, 0x00, 0x04, 0x00, 0x08, 0x00,
     0x10, 0x00, 0x20, 0x00, 0x40, 0x00, 0x80, 0x00
 ]
 
-curPos = [0x0,1]
+curPos = 0
 
 bus.write_i2c_block_data(matrix, 0, array)
 
@@ -101,8 +99,6 @@ while True:
     horzData = (horzEncoderCurData - horzEncoderOldData)
     vertData = (vertEncoderCurData - vertEncoderOldData)
     
-    print(vertData)
-    print(horzData)
     if(horzData > 0):
         move_right()
     elif (horzData < 0):
@@ -120,5 +116,5 @@ while True:
     vertEncoderOldData = vertEncoderCurData
     horzEncoderOldData = horzEncoderCurData
     
-    time.sleep(0.3)
+    time.sleep(delay)
 
