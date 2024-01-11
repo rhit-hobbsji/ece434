72c0eb1f366b8a69fed13dd1669b07d30af4649e
Hobbs <hobbsji@rose-hulman.edu>
2024-01-10 21:56:55 -0500

    Sensors Kernel Way

--- hw04/readTmp101.py
+++ hw04/readTmp101.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
-#Read a TMP101 sensor
+#Reads 2 TMP101 sensor
 #//////////////////////////////////////
 #Author : Jailen Hobbs
 #Program: Reads from address 0x48 and 0x49 of I2C bus 2 with kernel
 #HArdware configuration to pins 19 and 20 with one TMP101 GND and one floating
 #
-
-import smbus
 import time
 
-#i2c bus and addresses
-bus = smbus.SMBus(2)
-address = 0x48
-address2 = 0x49
+#enter dec value for ex address 0x48 put in 48
+hAddr = '48'
+hAddr2 = '49'
+
+#i2c bus
+i2cbus = '2'
+
+path1 = '/sys/class/i2c-adapter/i2c-'+i2cbus+'/' + i2cbus + '-00' + hAddr + 'hwmon/hwmon0'
+path2 = '/sys/class/i2c-adapter/i2c-'+i2cbus+'/' + i2cbus + '-00' + hAddr2 + 'hwmon/hwmon0'
+
+delay = 0.3
 
 while True:
 	#Get temp of both TMP101 sensors
-	temp = bus.read_byte_data(address, 0)
-	temp2 = bus.read_byte_data(address2, 0)
-	print(str(temp)+"-Sensor1 "+ str(temp2) + "-Sensor2", end = "\r")
-	time.sleep(0.1)
+    f = open(path1, 'r')
+    f.seek(0)
+    temp = int(f.read)
+    f.close()
+    f = open(path2, 'r')
+    f.seek(0)
+    temp2 = int(f.read)
+    f.close()
+    print(str(temp)+"-Sensor1 "+ str(temp2) + "-Sensor2", end = "\r")
+    time.sleep(delay)
